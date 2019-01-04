46a7b7df85213150ffb77ef9433352e401975b5f
jzy <528036346@qq.com>
2019-01-04 17:58:52 +0800

    Site updated: 2019-01-04 17:57:55

--- blog-album/ins.js
+++ blog-album/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/lawlite19/blog-back-up/master/mini_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/lawlite19/blog-back-up/master/photos/' + data.link[i];
+          var minSrc = 'https://github.com/Sophia0130/Sophia0130.github.io/tree/master/blog-album/mini_photos/' + data.link[i];
+          var src = 'https://github.com/Sophia0130/Sophia0130.github.io/tree/master/blog-album/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
